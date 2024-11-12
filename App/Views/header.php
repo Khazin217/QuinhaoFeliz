a145eb78442051242a3a200076d0667c75e57c6c
GreenFelipeJo <tecnicoinformatica.felipejose@gmail.com>
2024-11-12 19:46:59 -0300

    config head

--- App/Views/header.php
+++ App/Views/header.php
@@ -14,9 +14,11 @@
         </div>
         <nav> <!-- começo navegação -->
             <ul class="ul">
+                
                 <li><a href="index.html" >Home</a></li>
                 <li><a href="#footer" class="rolagem">Sobre Nos</a></li>
                 <li><a href="#footer" class="rolagem">Contato</a></li>
+
             </ul>
         </nav> <!-- fim navegação -->
         <div class="butoes">
